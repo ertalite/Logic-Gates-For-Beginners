2605521a5a6aa16dd82161ce544172dfc016356d
erta <2005mathys@gmail.com>
2023-04-16 18:29:14 +0200

    Ajout des fleches servant à se mouvoir

--- code/main.py
+++ code/main.py
@@ -12,9 +12,21 @@ clock = pygame.time.Clock()
 #-----------------------------[ Chargement des fichiers ]------------------
 
 main_menu_surface = com.Affichage.chargementFichier('./graphics/backgrounds/esthetique-minimal-violet.jpg')
+
+# Polices
+
 Nexa = com.Affichage.chargementPolice('./font/Nexa-Heavy.ttf', 30)
+
+# Textes 
+
 title_surface = com.Affichage.creerTexte(Nexa, 'Apprends les portes logiques !', True, 'black')
 title_rectangle = com.Collision.creerRectangle(title_surface, 'center', (640, 160))
+
+and_surface = com.Affichage.creerTexte(Nexa, 'AND', True, 'black')
+and_rectangle = com.Collision.creerRectangle(and_surface, 'center', (640, 160))
+
+or_surface = com.Affichage.creerTexte(Nexa, 'OR', True, 'black')
+or_rectangle = com.Collision.creerRectangle(or_surface, 'center', (640, 160))
 
 # Boutons de menu
 
@@ -48,10 +60,10 @@ b_button_pressed = misc.Bouton(350, 500, b_button_pressed_img, 1)
 
 #-----------------------------[ Game Loop ]------------------
 
-SYSTEM_STATE ='main'
-COURS_STATE = 'and'
+SYSTEM_STATE ='main' #Initialisation du menu principal au démarage du programme
+COURS_STATE = 'and' #Initalisation du menu "cours" sur la page "and"
 
-run = True
+run = True 
 while run:
     
     for event in pygame.event.get():
@@ -61,9 +73,11 @@ while run:
             sys.exit()
 
 
-    com.Affichage.afficher(screen, main_menu_surface, (0, 0))
+    com.Affichage.afficher(screen, main_menu_surface, (0, 0)) #Affichage du fond 
 
-    if SYSTEM_STATE == 'main':
+# Menu principal
+
+    if SYSTEM_STATE == 'main': 
 
         com.Affichage.afficher(screen, title_surface, title_rectangle)
 
@@ -76,6 +90,8 @@ while run:
         if quitter_button.afficher(screen) == True:
             run = False
 
+# Menu cours           
+
     if SYSTEM_STATE == 'cours':
 
         if retour_button.afficher(screen) == True:
@@ -83,8 +99,11 @@ while run:
             SYSTEM_STATE = 'main'
             COURS_STATE = None
 
+    # Onglet "and"
 
         if COURS_STATE == 'and':
+
+            com.Affichage.afficher(screen, and_surface, and_rectangle)
 
             keys = pygame.key.get_pressed()
 
@@ -103,6 +122,30 @@ while run:
    
             b_button.afficher(screen)
 
+    # Onglet "or"
+
+        if COURS_STATE == 'or':
+
+            com.Affichage.afficher(screen, or_surface, or_rectangle)
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_a] and keys[pygame.K_b]:
+                print("OK")
+
+            if keys[pygame.K_a]:
+
+                a_button_pressed.afficher(screen)
+
+            if keys[pygame.K_b]:
+
+                b_button_pressed.afficher(screen)
+
+            a_button.afficher(screen)
+   
+            b_button.afficher(screen)
+
+    # Menu circuit
 
     if SYSTEM_STATE == 'circuit':
 
